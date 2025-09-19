e0385a306bbbf8650f439be2d2058a61265dc1ff
thatguybk <bradleykhan22@gmail.com>
2025-09-19 03:50:03 -0400

    fixes

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -5,7 +5,7 @@ from App.main import create_app
 from App.database import db, create_db
 from App.models import User
 from App.controllers import (
-    create_user,
+    
     get_all_users_json,
     login,
     get_user,
