dde27d89d58a4e34171eea7a2ff6f8bdaafad3d7
xJGBx <91367019+xJGBx@users.noreply.github.com>
2025-10-23 20:38:36 -0400

    Add employer, staff, and student views to the main views list

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -6,7 +6,10 @@ from .auth import auth_views
 from .admin import setup_admin
 from .internship import api_internship_views
 from .shortlist import api_shortlist_views
+from .employer import api_employer_views
+from .staff import api_staff_views
+from .student import api_student_views
 
 
-views = [api_user_views, index_views, auth_views, api_internship_views, api_shortlist_views] 
+views = [api_user_views, index_views, auth_views, api_internship_views, api_shortlist_views, api_employer_views, api_staff_views, api_student_views] 
 # blueprints must be added to this list
